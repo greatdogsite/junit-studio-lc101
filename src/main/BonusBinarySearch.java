308c98b395c876dd56a3dc0daf7d1236b130cdc3
Mango <greatdogsite@gmail.com>
2023-04-10 20:19:03 -0500

    bonus complete

--- src/main/BonusBinarySearch.java
+++ src/main/BonusBinarySearch.java
@@ -12,17 +12,19 @@ public class BonusBinarySearch {
      * @return index of search item if it's found, -1 if not found
      */
     public static int binarySearch(int[] sortedNumbers, int n) {
-        int right = sortedNumbers.length - 1;
+        int right = sortedNumbers.length - 1; // 4
         int left = 0;
-        while (right >= left) {
-            int mid = left + ((right - left) / 2);
-            if (sortedNumbers[mid] > n) {
-                right = mid;
-            } else if (sortedNumbers[mid] < n) {
-                left = mid;
+        while (right >= left) {   //4 >= 0  //4>=2 // 4==4
+
+            int mid = left + (right - left) / 2;  //mid == 2 // 4 //
+            if (sortedNumbers[mid] > n) {  //false 3 >6
+                right = mid-1;
+            } else if (sortedNumbers[mid] < n) { // true 3<6
+                left = mid +1; //left == 2 // 4
             } else {
                 return mid;
             }
+
         }
         return -1;
     }
--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BonusBinarySearch;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,19 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+    @Test
+    public void numberIsPresent() {
+        int[] intArray = {1,2,3,4,5};
+        assertEquals(1, BonusBinarySearch.binarySearch(intArray,2));
+        assertEquals(4, BonusBinarySearch.binarySearch(intArray,5));
+
+    }
+    @Test
+    public void numberIsNotPresent() {
+        int[] intArray = {1,2,3,4,5};
+        assertEquals(-1, BonusBinarySearch.binarySearch(intArray,-1));
+        assertEquals(-1, BonusBinarySearch.binarySearch(intArray,6));
     }
 
 
